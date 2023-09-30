050e004f98f10883d1e634972e10cce1e34d0f77
Krzysztof Łazarz <krzysztof.lazarz.dev@gmail.com>
2023-09-30 21:48:32 +0200

    api inflated

--- kryptokasa/lib/api/api.dart
+++ kryptokasa/lib/api/api.dart
@@ -1,6 +1,22 @@
 import 'dart:async';
+import 'package:kryptokasa/api/npb.dart';
+import 'package:kryptokasa/api/zonda.dart';
+
 import 'exchangeInfo.dart';
 
-List<Future<ExchangeInfo?>Function()> markets = [
-
+//List representing all available markets
+List<Future<ExchangeInfo?>Function(String inputCurrency)> markets = [
+  NBPMarket().getValueInfo,
+  ZondaMarket().getValueInfo
 ];
+
+
+class CryptoTask{ // class reprezenting UI input panel
+  late List<CryptoPair> cryptoPairs;
+}
+
+class CryptoPair{ // class representing single input row
+  String inputCurrency;
+  String amount;
+  CryptoPair(this.inputCurrency, this.amount);
+}
