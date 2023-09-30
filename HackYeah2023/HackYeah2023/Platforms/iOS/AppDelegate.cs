2a5b766983d22157c673c058c8dc7a43059ee1e1
QuantuPL <Quantu.Jaworski@gmail.com>
2023-09-30 16:32:40 +0200

    init

---
+++ HackYeah2023/HackYeah2023/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HackYeah2023;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
